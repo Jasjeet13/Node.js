b2bc2f0a25a74c10b7d6b0831e171af2cb1619b9
Jasjeet <jasjeet13.saini@gmail.com>
2024-08-12 11:56:16 +0530

    printing

---
+++ print.js
@@ -0,0 +1,16 @@
+/* 
+-- STEP : 1
+    -> run npm.init in the terminal
+    -> package.json file will be created
+    
+-- STEP : 2
+    -> Run your code by using command <node filename> or <node filename.js>
+
+-- STEP : 3 (ALTERNATIVE FOR STEP 2)
+    -> Give a specific script in package.json file
+    -> Example start : 'node print.js'
+    -> then run the command npm start
+*/
+
+console.log("Hello World")
+
